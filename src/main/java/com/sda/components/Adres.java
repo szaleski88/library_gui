4dc1f07579331bd0f87a2f07ffc93ddc3075af5d
Sebastian Zaleski <s.zaleski88@gmail.com>
2017-12-10 11:56:42 +0100

    Dodany adres + uzytkownik

---
+++ src/main/java/com/sda/components/Adres.java
@@ -0,0 +1,104 @@
+package com.sda.components;
+
+public class Adres implements Comparable<Adres>{
+
+    private  String ulica;
+    private  int numerBudynku;
+    private  String kodPocztowy;
+    private String miejscowosc;
+
+
+    public Adres(String ulica, int numerBudynku, String miejscowosc, String kodPocztowy) {
+        setUlica(ulica);
+        setMiejscowosc(miejscowosc);
+        setKodPocztowy(kodPocztowy);
+        setNumerBudynku(numerBudynku);
+    }
+
+    public String getUlica() {
+        return ulica;
+    }
+
+    public void setUlica(String ulica) {
+        this.ulica = ulica;
+    }
+
+    public int getNumerBudynku() {
+        return numerBudynku;
+    }
+
+    public void setNumerBudynku(int numerBudynku) {
+        this.numerBudynku = numerBudynku;
+    }
+
+    public String getKodPocztowy() {
+        return kodPocztowy;
+    }
+
+    public void setKodPocztowy(String kodPocztowy) {
+        this.kodPocztowy = kodPocztowy;
+    }
+
+    public String getMiejscowosc() {
+        return miejscowosc;
+    }
+
+    public void setMiejscowosc(String miejscowosc) {
+        this.miejscowosc = miejscowosc;
+    }
+
+
+    public int compareTo(Adres o) {
+        if( this.getUlica().compareToIgnoreCase(o.getUlica() ) == 0) {
+            if (this.getNumerBudynku() >  o.getNumerBudynku()) {
+                return 1;
+            } else if (this.getNumerBudynku() < o.getNumerBudynku())  return -1;
+            else return 0;
+        }
+
+        else return this.getUlica().compareToIgnoreCase(o.getUlica());
+
+    }
+
+//    @Override
+//    public String toString() {
+//        return "Adres{" +
+//                "ulica='" + ulica + '\'' +
+//                ", numerBudynku=" + numerBudynku +
+//                ", miejscowosc='" + miejscowosc + '\'' +
+//                '}';
+//    }
+
+
+    @Override
+    public String toString() {
+        final StringBuffer sb = new StringBuffer("");
+        sb.append(ulica);
+        sb.append(" ").append(numerBudynku);
+        sb.append(", ").append(kodPocztowy);
+        sb.append(" ").append(miejscowosc);
+        return sb.toString();
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Adres adres = (Adres) o;
+
+        if (numerBudynku != adres.numerBudynku) return false;
+        if (!ulica.equals(adres.ulica)) return false;
+        if (!kodPocztowy.equals(adres.kodPocztowy)) return false;
+        return miejscowosc.equals(adres.miejscowosc);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = ulica.hashCode();
+        result = 31 * result + numerBudynku;
+        result = 31 * result + kodPocztowy.hashCode();
+        result = 31 * result + miejscowosc.hashCode();
+        return result;
+    }
+}
--- src/main/java/com/sda/components/Uzytkownik.java
+++ src/main/java/com/sda/components/Uzytkownik.java
@@ -1,4 +1,67 @@
 package com.sda.components;
 
-public class Uzytkownik {
+public class Uzytkownik implements  Comparable<Uzytkownik>{
+
+    private String imie;
+    private String nazwisko;
+    private Adres adres;
+
+
+    public Uzytkownik(String imie, String nazwisko, Adres adres) {
+        this.imie = imie;
+        this.nazwisko = nazwisko;
+        this.adres = adres;
+    }
+
+
+    public String getImie() {
+        return imie;
+    }
+
+    public void setImie(String imie) {
+        this.imie = imie;
+    }
+
+    public String getNazwisko() {
+        return nazwisko;
+    }
+
+    public void setNazwisko(String nazwisko) {
+        this.nazwisko = nazwisko;
+    }
+
+    @Override
+    public String toString() {
+        final StringBuffer sb = new StringBuffer("Uzytkownik{");
+        sb.append("imie='").append(imie).append('\'');
+        sb.append(", nazwisko='").append(nazwisko).append('\'');
+        sb.append(", adres=").append(adres);
+        sb.append('}');
+        return sb.toString();
+    }
+
+
+    public int compareTo(Uzytkownik o) {
+       return 0;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Uzytkownik that = (Uzytkownik) o;
+
+        if (!imie.equals(that.imie)) return false;
+        if (!nazwisko.equals(that.nazwisko)) return false;
+        return adres.equals(that.adres);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = imie.hashCode();
+        result = 31 * result + nazwisko.hashCode();
+        result = 31 * result + adres.hashCode();
+        return result;
+    }
 }
